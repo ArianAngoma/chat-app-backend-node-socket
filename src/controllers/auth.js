5c181386f2047c5ae9c8f38a850978d985c90491
unknown <arian.angoma@tecsup.edu.pe>
2021-11-08 04:44:40 -0500

    Encriptar password de usuario

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 /* Importaciones propias */
 const User = require('../models/User');
 
@@ -7,6 +9,10 @@ const registerUser = async (req, res) => {
 
     try {
         const user = new User({name, email, password});
+
+        /* Encriptar password */
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
 
         /* Guardar en DB */
         await user.save();
