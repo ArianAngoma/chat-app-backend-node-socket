531db9007837e0214d7d4f7eb1a707adb586615d
unknown <arian.angoma@tecsup.edu.pe>
2021-11-08 03:58:02 -0500

    Validaciones en las rutas del auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,15 +8,27 @@ const {check} = require('express-validator');
 
 /* Importaciones propias */
 const {registerUser, loginUser, renewToken} = require('../controllers/auth');
+const {emailExists} = require('../helpers/db-validators');
+const {validateFields} = require('../middlewares/validate-fields');
 
 /* Configuración del router */
 const router = Router();
 
 /* Registrar usuario */
-router.post('/register', registerUser);
+router.post('/register', [
+    check('name', 'El nombre es obligatorio').notEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('email').custom(emailExists),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({min: 6}),
+    validateFields
+], registerUser);
 
 /* Iniciar sesión de usuario */
-router.post('/', loginUser);
+router.post('/', [
+    check('email', 'El email es obligatorio').notEmpty(),
+    check('password', 'El password es obligatorio').notEmpty(),
+    validateFields
+], loginUser);
 
 /* Revalidar Token */
 router.get('/renew', renewToken);
